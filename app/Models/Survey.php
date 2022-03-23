bc5092d2f83590186597f0b8a3d1cf32d22a40f7
quangtao <tao.nguyenquang@evotek.vn>
2022-03-23 16:42:52 +0700

    survey

---
+++ app/Models/Survey.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Survey extends Model
+{
+    use HasFactory;
+    protected $table = 'survey';
+    protected $fillable = [
+        'name',
+        'type',
+        'content',
+        'rate',
+        'admin_id',
+        // 'customer_id', 
+        // 'admin_id', 
+        // 'status', 
+        // 'rate', 
+        // 'content', 
+        // 'email', 
+        // 'phone', 
+        // 'customer_name'
+    ];
+    //query search
+    protected $searchableByOrWhere = [
+    ];
+    public function getSearchableByOrWhere(){
+        return $this->searchableByOrWhere;
+    }    
+    
+
+    protected $searchableByWhere = [
+        'name',
+        'type'
+    ];    
+    public function getSearchableByWhere(){
+        return $this->searchableByWhere;
+    } 
+
+    // column table
+    protected $columnTable = [
+        'name',
+        'type'
+    ];    
+    public function getColumnTable(){
+        return $this->columnTable;
+    } 
+    //column sorting
+    protected $columnSortingTable = [
+        'name',
+        'type'
+    ];    
+    public function getColumnSortingTable(){
+        return $this->columnSortingTable;
+    } 
+
+    
+    //export
+    protected $columnExport = [
+        'name',
+        'type'
+    ];  
+    public function  getColumnExport() {
+        return $this->columnExport;
+    }
+
+    // translate
+    protected $translate = [
+        'name' => 'Tên khảo sát',
+        'type' => 'Loại khảo sát',
+    ];
+    public function getTranslate(){
+        return $this->translate;
+    }
+}
