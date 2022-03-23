592f45e4ef8dd506ca888adf103380fcf98061ef
manh.trantien <manh.trantien@evotek.vn>
2022-03-23 14:57:53 +0700

    change-icon-sidebar

--- resources/views/layouts/partials/_sidebar.blade.php
+++ resources/views/layouts/partials/_sidebar.blade.php
@@ -25,7 +25,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item {{setOpen('guideline')}}{{setOpen('master')}}">
             <a href="#" class="nav-link">
-              <i class="nav-icon far fa-envelope"></i>
+              <i class=" nav-icon fas fa-cog"></i>
               <p>
                 Cấu hình
                 <i class="fas fa-angle-left right"></i>
@@ -78,13 +78,13 @@
           </li> --}}
           <li class="nav-item active">
             <a href="{{route('admin.customer.index')}}" class="nav-link {{setActive2('customer')}}">
-                <i class="nav-icon fas fa-user-alt"></i>
+                <i class="nav-icon fas fa-users"></i>
                 <p>Quản lý khách hàng</p>
             </a>
         </li>
           <li class="nav-item {{setOpen('ticker')}} {{setOpen('role')}}">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-ticket-alt"></i>
               <p>Quản lý ticket</p>
               <i class="fas fa-angle-left right"></i>
             </a>
@@ -99,7 +99,7 @@
           </li>
           <li class="nav-item {{setOpen('sla')}}">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-user"></i>
+              <i class="nav-icon fas fa-signal"></i>
               <p>Quản lý SLA</p>
               <i class="fas fa-angle-left right"></i>
             </a>
@@ -121,19 +121,19 @@
           </li>
           <li class="nav-item active">
             <a href="{{route('admin.serviceProduct.index')}}" class="nav-link {{setActive2('serviceProduct')}}">
-              <i class="nav-icon fas fa-file"></i>
+              <i class="nav-icon fas fa-tools"></i>
               <p>Sản phẩm dịch vụ</p>
             </a>
           </li>
             <li class="nav-item active">
                 <a href="{{route('admin.delegate.index')}}" class="nav-link {{setActive2('delegate')}}">
-                    <i class="nav-icon fas fa-user-alt"></i>
+                    <i class="nav-icon fas fa-user-tie"></i>
                     <p>Người đại diện</p>
                 </a>
             </li>
           <li class="nav-item active">
             <a href="{{route('admin.unit.index')}}" class="nav-link {{setActive2('unit')}}">
-              <i class="nav-icon fas fa-file"></i>
+              <i class="nav-icon fas fa-building"></i>
               <p>Quản lý đơn vị</p>
             </a>
           </li>
